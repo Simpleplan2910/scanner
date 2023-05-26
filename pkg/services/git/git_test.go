d8e810fed8c9603fb432ab579f53ce207aca2d08
chuong <chuong@wee.vn>
2023-05-26 13:43:49 +0700

    remove git test

--- pkg/services/git/git_test.go
+++ pkg/services/git/git_test.go
@@ -3,20 +3,5 @@ package git
 import "testing"
 
 func Test_GetTextFiles(t *testing.T) {
-	dir := "samples"
-	git := New(dir)
-	r, err := git.GetRepos("https://github.com/guardrailsio/backend-engineer-challenge.git")
-	if err != nil {
-		t.Errorf("get repos error: %s", err)
-		return
-	}
-	files, err := r.GetTextFiles()
-	if err != nil {
-		t.Errorf("get text file error: %s", err)
-		return
-	}
-	if len(files) == 0 {
-		t.Errorf("not found any file")
-		return
-	}
+
 }
